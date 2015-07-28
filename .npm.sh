2a9c194d27b75bfbad07797d2f5d6a2529148623
John Otander <johnotander@gmail.com>
2015-07-28 10:47:48 -0600

    Add semi and jshint

--- .npm.sh
+++ .npm.sh
@@ -4,7 +4,7 @@ npm i -g yo bower grunt-cli gulp
 
 npm i -g generator-p generator-c generator-index generator-license generator-rework generator-generator
 
-npm i -g cssstats scrutinize get-css get-contrast a11y psi immutable-css
+npm i -g cssstats scrutinize get-css get-contrast a11y psi immutable-css semi jshint
 
 npm i -g whois-cli is-up wifi-password wifi-name brightness-cli vol battery-level pure-prompt
 
