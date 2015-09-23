75064373b33d78ef449b4cbef9a60e1417fcecc6
John Otander <johnotander@gmail.com>
2015-09-22 18:24:57 -0600

    Attempt to link the pure prompt

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,3 +9,6 @@ git pull origin master;
 rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
   --exclude "README.md" --exclude "LICENSE" -avh --no-perms . ~;
 source ~/.bash_profile;
+
+ln -s "./pure/pure.zsh" /usr/local/share/zsh/site-functions/prompt_pure_setup
+ln -s "./pure/async.zsh" /usr/local/share/zsh/site-functions/async
