39a1f3c8a766e0053b79b1bd7fdf075573d8d053
John Otander <johnotander@gmail.com>
2015-09-23 15:38:31 -0600

    Add the trash module

--- .npm.sh
+++ .npm.sh
@@ -8,6 +8,6 @@ npm i -g cssstats scrutinize get-css get-contrast a11y psi immutable-css semi js
 
 npm i -g postcss perfectionist postcss-remove-prefixes cssnext
 
-npm i -g whois-cli is-up wifi-password wifi-name brightness-cli vol battery-level pure-prompt empty-trash do-not-disturb
+npm i -g whois-cli is-up wifi-password wifi-name brightness-cli vol battery-level pure-prompt trash empty-trash do-not-disturb
 
 npm i -g gh github-search-repos github-repositories
