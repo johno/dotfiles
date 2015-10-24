deb7344a2e389c1efbabe607336e7e662695f14d
John Otander <johnotander@gmail.com>
2015-10-24 10:34:39 -0600

    Update npm script

--- .npm.sh
+++ .npm.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-npm i -g npm yo bower grunt-cli gulp turn-it-off-and-on-again electron-prebuilt
+npm cache clean
+npm i -g npm
+
+npm i -g yo bower grunt-cli gulp turn-it-off-and-on-again electron-prebuilt
 
 npm i -g generator-p generator-c generator-index generator-rework
 
@@ -8,6 +11,6 @@ npm i -g cssstats scrutinize get-css get-contrast a11y psi immutable-css semi js
 
 npm i -g postcss perfectionist postcss-remove-prefixes cssnext
 
-npm i -g whois-cli is-up wifi-password wifi-name brightness-cli vol battery-level pure-prompt trash empty-trash do-not-disturb
+npm i -g whois-cli is-up wifi-password wifi-name brightness-cli vol battery-level pure-prompt trash-cli empty-trash-cli do-not-disturb
 
 npm i -g gh github-search-repos github-repositories
