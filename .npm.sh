f015327ca05284ba33b72dcd46d4b980ffc167af
John Otander <johnotander@gmail.com>
2015-10-26 16:36:52 -0600

    Update some package names

--- .npm.sh
+++ .npm.sh
@@ -7,10 +7,10 @@ npm i -g yo bower grunt-cli gulp turn-it-off-and-on-again electron-prebuilt
 
 npm i -g generator-p generator-c generator-index generator-rework
 
-npm i -g cssstats scrutinize get-css get-contrast a11y psi immutable-css semi jshint sri-cli np
+npm i -g cssstats scrutinize get-css get-contrast a11y psi immutable-css-cli semi jshint sri-cli np
 
 npm i -g postcss perfectionist postcss-remove-prefixes cssnext
 
-npm i -g whois-cli is-up wifi-password wifi-name brightness-cli vol battery-level pure-prompt trash-cli empty-trash-cli do-not-disturb
+npm i -g whois-cli is-up-cli wifi-password wifi-name brightness-cli vol battery-level pure-prompt trash-cli empty-trash-cli do-not-disturb
 
 npm i -g gh github-search-repos github-repositories
