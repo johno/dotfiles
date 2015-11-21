4cbee7a75a0e32a57faec52d66e193811a3d3c87
John Otander <johnotander@gmail.com>
2015-11-21 13:07:29 -0700

    Add a few more cli tools

--- .npm.sh
+++ .npm.sh
@@ -14,3 +14,5 @@ npm i -g postcss perfectionist postcss-remove-prefixes cssnext
 npm i -g whois-cli is-up-cli wifi-password wifi-name brightness-cli vol battery-level pure-prompt trash-cli empty-trash-cli do-not-disturb
 
 npm i -g gh github-search-repos github-repositories
+
+npm i -g strip-comments-cli css-scss-cli get-embedded-styles-cli
