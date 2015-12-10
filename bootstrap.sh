db501697ba485f2a86be44e2fc03cb63d10fef60
John Otander <johnotander@gmail.com>
2015-12-10 15:25:36 -0700

    Tweak bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,10 +7,8 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-  --exclude "README.md" --exclude "LICENSE" -avh --no-perms . ~;
+  --exclude "readme.md" --exclude "license" -avh --no-perms . ~;
 
 fpath=( "$HOME/.zfunctions" $fpath )
 ln -sf ~/.pure.zsh ~/.zfunctions/prompt_pure_setup
 ln -sf ~/.async.zsh ~/.zfunctions/async
-
-source ~/.bash_profile;
