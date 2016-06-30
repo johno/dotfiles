3a9a40e6bea001260f4b8172d99e2eed6062f715
John Otander <johnotander@gmail.com>
2016-06-29 23:19:03 -0600

    Tweak npm packages

--- .npm.sh
+++ .npm.sh
@@ -3,18 +3,20 @@
 npm cache clean
 npm i -g npm
 
-npm i -g yo bower grunt-cli gulp turn-it-off-and-on-again electron-prebuilt
+npm i -g turn-it-off-and-on-again electron-prebuilt
 
-npm i -g generator-p generator-c generator-index generator-rework
+npm i -g yo generator-p generator-c generator-index generator-rework generator-p-cli
 
-npm i -g cssstats scrutinize get-css get-contrast a11y psi immutable-css-cli semi jshint sri-cli np
+npm i -g cssstats-cli scrutinize get-css get-contrast immutable-css-cli sri-cli whois-cli
 
-npm i -g postcss perfectionist postcss-remove-prefixes cssnext
+npm i -g postcss-remove-prefixes
 
-npm i -g whois-cli is-up-cli wifi-password wifi-name brightness-cli vol battery-level pure-prompt trash-cli empty-trash-cli do-not-disturb
+npm i -g is-up-cli wifi-password wifi-name brightness-cli vol battery-level pure-prompt trash-cli empty-trash-cli do-not-disturb npm-name
 
 npm i -g gh github-search-repos github-repositories
 
 npm i -g strip-html-comments-cli css-scss-cli get-embedded-styles-cli
 
-npm i -g git-current-branch git-current-repo ggpull ggpush ggpullr ggpusht ghclone gtag github-latest-cli
+npm i -g git-current-branch git-current-repo ggpull ggpush ggpullr ggpusht ghclone gtag github-latest-cli ghremote
+
+npm i -g naka ember-cli rj-cli
